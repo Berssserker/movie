806c604c5ae67b98a2f13eb43c4b43dc1334e181
Berssserker <razdumkov1804@gmail.com>
2024-10-23 00:39:46 +0300

    1231

--- src/app/movie-list/movie-card/movie-card.js
+++ src/app/movie-list/movie-card/movie-card.js
@@ -1,33 +1,38 @@
 import React, { Component } from 'react'
-import { Card } from 'antd'
-import { format } from 'date-fns'
+import { Card, Spin } from 'antd'
+// import { format } from 'date-fns'
 
 import './movie-card.css'
 
 export default class MovieCard extends Component {
+  state = {
+    loading: true,
+  }
+
   render() {
-    const { Meta } = Card
+    // const { Meta } = Card
     return (
       <Card
-        cover={
-          <img
-            alt="nenь"
-            src={
-              this.props.poster_path !== null
-                ? `https://image.tmdb.org/t/p/original${this.props.poster_path}`
-                : 'https://avatars.mds.yandex.net/get-entity_search/2320096/993364518/S600xU_2x'
-            }
-          />
-        }
+      // cover={
+      //   <img
+      //     alt="nenь"
+      //     src={
+      //       this.props.poster_path !== null
+      //         ? `https://image.tmdb.org/t/p/original${this.props.poster_path}`
+      //         : 'https://avatars.mds.yandex.net/get-entity_search/2320096/993364518/S600xU_2x'
+      //     }
+      //   />
+      // }
       >
-        <Meta title={this.props.title} description={this.props.overview !== '' ? this.props.overview : 'nenь'} />
+        <Spin size="large" />
+        {/* <Meta title={this.props.title} description={this.props.overview !== '' ? this.props.overview : 'nenь'} />
         <span className="date">
           {this.props.release_date !== '' ? format(new Date(this.props.release_date), 'MMMM d, yyyy') : 'nenь'}
         </span>
         <div className="genre">
           <span>Action</span>
           <span>Drama</span>
-        </div>
+        </div> */}
       </Card>
     )
   }
