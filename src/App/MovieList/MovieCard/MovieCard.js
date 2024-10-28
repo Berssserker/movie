8b7f5c08e6be73f981d5634be2b8b9059be1eac5
Berssserker <razdumkov1804@gmail.com>
2024-10-28 03:46:15 +0300

    123123

--- src/App/MovieList/MovieCard/MovieCard.js
+++ src/App/MovieList/MovieCard/MovieCard.js
@@ -4,20 +4,27 @@ import { format } from 'date-fns'
 
 import './MovieCard.css'
 
-const MovieCard = ({ poster_path, overview, title, release_date }) => {
+const MovieCard = (props) => {
   const [loading, setLoading] = useState(true)
   const { Meta } = Card
   const url = 'https://image.tmdb.org/t/p/original'
   useEffect(() => {
-    if (poster_path || poster_path === '') {
+    if (props) {
       setLoading(false)
     }
-  }, [poster_path])
+  }, [props])
+  if (loading) {
+    return (
+      <Card>
+        <Spin size="large" />
+      </Card>
+    )
+  }
   return (
-    <Card cover={loading ? <Spin size="large" /> : <img src={url + poster_path} />}>
-      <Meta title={title} description={overview !== '' ? overview : 'Description not found :<'} />
+    <Card cover={<img alt="Poster" src={url + props.poster_path} />}>
+      <Meta title={props.title} description={props.overview !== '' ? props.overview : 'Description not found :<'} />
       <div className="date">
-        {release_date !== '' ? format(new Date(release_date), 'MMMM d, yyyy') : 'Date not found :<'}
+        {props.release_date !== '' ? format(new Date(props.release_date), 'MMMM d, yyyy') : 'Date not found :<'}
       </div>
       <div className="genre">
         <span>Action</span>
