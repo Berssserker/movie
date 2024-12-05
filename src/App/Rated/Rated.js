63c2782631b9a50e849ecce2c2617f8a6d8e60e1
Berssserker <razdumkov1804@gmail.com>
2024-12-05 23:36:01 +0300

    asdfasdfa

--- src/App/Rated/Rated.js
+++ src/App/Rated/Rated.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
+import './Rated.css'
+
 import MovieList from './MovieList/MovieList'
 
 const Rated = ({ setMovieId, setRate, guestId, ratedMoviesData }) => {
-  return <MovieList setMovieId={setMovieId} setRate={setRate} guestId={guestId} ratedMoviesData={ratedMoviesData} />
+  return (
+    <div className="rated">
+      <MovieList setMovieId={setMovieId} setRate={setRate} guestId={guestId} ratedMoviesData={ratedMoviesData} />
+    </div>
+  )
 }
 
 export default Rated
