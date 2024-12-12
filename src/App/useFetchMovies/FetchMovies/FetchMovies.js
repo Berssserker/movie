1aa1b8830039c5ec24d8776e5a8e62a6ba11cbe2
Semion <razdumkov1804@gmail.com>
2024-12-12 22:06:33 +0300

    asdasd

--- src/App/useFetchMovies/FetchMovies/FetchMovies.js
+++ src/App/useFetchMovies/FetchMovies/FetchMovies.js
@@ -3,15 +3,11 @@ import GetMovie from './GetMovie/GetMovie'
 const FetchMovies = async (setMoviesData, text, setError, page) => {
   setError(false)
   const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
-  try {
-    await delay(3000)
-    const data = await GetMovie(text, page)
-    setMoviesData(data.results || [])
-    if (data.results !== null && data.results.length === 0 && text) {
-      setError(true)
-    }
-  } catch (error) {
-    setMoviesData([])
+  await delay(3000)
+  const data = await GetMovie(text, page)
+  setMoviesData(data.results || [])
+  if (data.results.length === 0 && text) {
+    setError(true)
   }
 }
 
